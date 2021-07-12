c02114aa3e4e20eece6aba2c7587e9d347352dc2
Daniela Andrei <daniela.andrei@trencadis.ro>
2021-07-12 12:29:59 +0300

    Fifth commit

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { RecipeModel } from "../../recipe.model";
-import {RecipeService} from "../../recipe.service";
+
 
 @Component({
   selector: 'app-recipe-item',
@@ -9,14 +9,11 @@ import {RecipeService} from "../../recipe.service";
 })
 export class RecipeItemComponent implements OnInit {
     @Input() recipe: RecipeModel;
+    @Input() index: number;
 
 
-  constructor(private recipeService: RecipeService) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
   }
-  onSelectRecipe(){
-    this.recipeService.recipeSelected.emit(this.recipe);
 
-  }
 }
